5860bff2aed4cee2bc8118b84ef1524ef065cc6b
ophir raz <raz4ophir@gmail.com>
2019-07-23 18:06:38 +0300

    ADDED A SHIP

--- game.py
+++ game.py
@@ -2,22 +2,21 @@ import pygame
 import random
 
 BACKROUND = (123, 189, 189)
-LEANGTH = 500
-MAX_ROCKS = 10
-PLAYER_SIZE = 25
-ROCK_WIDTH = 30
-ROCK_HIGHT = 30
-MAX_ROCK_SIZE = 70
+WIDH = 1024
+HIGHT = 600
+MAX_ROCKS = 7
+PLAYER_SIZE = 100
+MAX_ROCK_SIZE = 150
+MINIMUM_ROCK_SIZE = 10
 
 def getRandomRockSize():
-    return random.randint(5,MAX_ROCK_SIZE+1)
+    return random.randint(MINIMUM_ROCK_SIZE, MAX_ROCK_SIZE+1)
 
 def getRandomX():
-    return random.randint(0,LEANGTH+1)
+    return random.randint(0,WIDH+1)
 
 def isDead(player, rocks):
     Dead = False
-    playerBlock = []
     for r in rocks:
         if r.y+r.SIZE >= player.y and player.x <= r.x+r.SIZE and player.x+PLAYER_SIZE >= r.x and r.y <= player.y+PLAYER_SIZE:
             Dead = True
@@ -28,15 +27,15 @@ def moveRocks(rocks, win):
     delete = []
     for r in rocks:
         r.y += r.pase
-        r.pase += r.pase / 7
-        if r.y < LEANGTH :
+        r.pase += r.pase / 10
+        if r.y < HIGHT :
             pygame.draw.rect(win, r.color, (r.x, r.y, r.SIZE, r.SIZE))
         else:
             delete.append(r)
     return delete
 
 class rock:
-    x = 250
+    x = 0
     y = 0
     SIZE = 0
     pase = 0
@@ -50,12 +49,16 @@ class player:
     pase = 7
     color = (70, 20, 10)
 
+
+
+
 def main():
     pygame.init()
+    shipImage = pygame.image.load('pictues/battleShip.png')
     #startMenue()
     player1 = player()
-    win = pygame.display.set_mode((LEANGTH, LEANGTH))
-    pygame.display.set_caption("Try To Survive...")
+    win = pygame.display.set_mode((WIDH, HIGHT))
+    pygame.display.set_caption("TheRolingStones...")
     rocks = []
     run = True
     while run:
@@ -68,11 +71,11 @@ def main():
 
         if keys[pygame.K_LEFT] and player1.x > player1.pase:
             player1.x -= player1.pase
-        if keys[pygame.K_RIGHT] and player1.x < LEANGTH - player1.width - player1.pase:
+        if keys[pygame.K_RIGHT] and player1.x < WIDH - player1.width - player1.pase:
             player1.x += player1.pase
         if keys[pygame.K_UP] and player1.y > player1.pase:
             player1.y -= player1.pase
-        if keys[pygame.K_DOWN] and player1.y < (LEANGTH - player1.hight - player1.pase):
+        if keys[pygame.K_DOWN] and player1.y < (HIGHT - player1.hight - player1.pase):
             player1.y += player1.pase
 
         if len(rocks) <= MAX_ROCKS:
@@ -89,7 +92,10 @@ def main():
         for r in delete:
             rocks.remove(r)
 
-        pygame.draw.rect(win, player1.color, (player1.x, player1.y, player1.width, player1.hight))
+        #pygame.draw.rect(win, player1.color, (player1.x, player1.y, player1.width, player1.hight))
+
+        win.blit(shipImage, (player1.x,player1.y))
+
         pygame.display.update()
         if(isDead(player1, rocks)):
             run = False
@@ -99,15 +105,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-"""
-for x in range(player.x, player.x+PLAYER_SIZE):
-        for y in range(player.y, player.y+PLAYER_SIZE):
-            playerBlock.append((x, y))
-    for r in rocks:
-        for x in range(r.x, r.x+ROCK_WIDTH):
-            for y in range(r.y, r.y+ROCK_HIGHT):
-                for point in playerBlock:
-                    if (x, y) == point:
-                        Dead = True
-                        break
-"""
+
